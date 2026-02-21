9b8687b67549ccbb0f091934dea0c81444d9566b
codedbyelif <elifkyr3@gmail.com>
2026-02-21 12:27:38 +0300

    feat: use the new logo image asset

--- src/components/KittyLogo.tsx
+++ src/components/KittyLogo.tsx
@@ -7,7 +7,7 @@ interface Props {
 export default function KittyLogo({ size = 48 }: Props) {
     return (
         <Image
-            src="/Adsız tasarım.png"
+            src="/Adsız tasarım kopyası.png"
             alt="Kitty Logo"
             width={size * 1.75}
             height={size * 1.75}
